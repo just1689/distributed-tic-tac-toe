0b199639ccfca1a925d26925aff776971e03a9fe
Just1689 <Just1689@gmail.com>
2020-01-03 19:35:34 +0200

    support for specifying nats server url

--- bin/gateway.go
+++ bin/gateway.go
@@ -4,15 +4,17 @@ import (
 	"flag"
 	"github.com/just1689/distributed-tic-tac-toe/config"
 	"github.com/just1689/distributed-tic-tac-toe/server"
-	"github.com/just1689/swoq/swoq"
+	"github.com/just1689/swoq/queue"
 	"github.com/sirupsen/logrus"
 )
 
 var listen = flag.String("listen", ":8080", "listen address")
+var n = flag.String("nats", "nats://127.0.0.1:4222", "The NATS url (defaults to nats://127.0.0.1:4222) for a NATS server instance.")
 
 func main() {
 	logrus.Println("Starting...")
-	swoq.StartQueueClient()
+	flag.Parse()
+	queue.BuildDefaultConnFromUrl(*n)
 	server.StartWS(config.GetVar("listen", *listen), server.IncomingOnlyOnce)
 
 }
