cc7696b3db4aa6a7ba27c32fb4ac9e43d89cc1f5
Just1689 <Just1689@gmail.com>
2020-01-07 22:03:50 +0200

    trying with sudo

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+#APP_NAME=$(cat config/APP_NAME)
+#RUN_AS="kubectl apply -f build/${CLUSTER}-${APP_NAME}.yaml"
+#echo Running: $RUN_AS
+#sudo su - ${CLUSTER} bash -c "${RUN_AS}"
+
 APP_NAME=$(cat config/APP_NAME)
-RUN_AS="kubectl apply -f build/${CLUSTER}-${APP_NAME}.yaml"
-echo Running: $RUN_AS
-sudo su - ${CLUSTER} bash -c "${RUN_AS}"
+kubectl apply -f build/${CLUSTER}-${APP_NAME}.yaml
